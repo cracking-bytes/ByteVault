8740195bcc535f1d9802cbf559427dc0579a33c5
Bhavika Nagdeo <alien@Brainiac.home>
2025-11-01 08:15:12 +0530

    CLI Styling and bug fixes

--- src/main.py
+++ src/main.py
@@ -3,6 +3,21 @@ import os
 import mysql.connector as msc
 
 
+# colours
+class c:
+   P = '\033[95m' #purple
+   C = '\033[96m' #cyan
+   DC = '\033[36m' #darkcyan
+   B = '\033[94m' #blue
+   G = '\033[92m' #green
+   Y = '\033[93m' #yellow
+   R = '\033[91m' #red
+   BD = '\033[1m' #bold
+   UL = '\033[4m' #underline
+   E = '\033[0m' #end
+
+
+
 # ---------- encryption setup
 
 basedir = os.path.dirname(os.path.abspath(__file__))
@@ -17,7 +32,7 @@ def genkey():
     with open(kpath, 'wb') as keyf:
         keyf.write(key)
     
-    print(" + New encryption key generated")
+    print(c.G + "\n- New encryption key generated -" + c.E)
 
 def loadk():
     # loading existing fernet keyy from file
@@ -47,6 +62,7 @@ def unlock(token:bytes) -> str:
     return fernet.decrypt(token).decode()
 
 
+
 # ---------- mysql connection
 
 def connect_db():
@@ -54,28 +70,29 @@ def connect_db():
         con = msc.connect(
             host = "localhost",
             user = "root",
-            password = "",
+            password = input(c.P + "\nEnter MySQL password: " + c.E), 
             database = "bytevault"
         )
 
         cur = con.cursor()
-        print(" + Connected to MySQL")
+        print(c.G + "\n- Connected to MySQL -" + c.E)
         return con, cur
     
     except Exception as e:
-        print(" - Database connection failed", e)
+        print(c.R + "\n! Database connection failed !", e, + c.E)
         exit()
 
 con, cur = connect_db()
 
 
+
 # ---------- master password
 
 def setp():
-    pw = input("Set a master password: ")
+    pw = input(c.P + "Set a master password: " + c.E)
     cur.execute(r"INSERT INTO master (id, master_pass) VALUES (1, %s)", (lock(pw),))
     con.commit()
-    print(" + Master password set successfully")
+    print(c.G + "\n- Master password set successfully -" + c.E)
 
 def chkmpw():
     cur.execute("SELECT master_pass FROM master WHERE id=1")
@@ -84,44 +101,46 @@ def chkmpw():
         setp()
         return True
     else:
-        pw = input("Enter master password: ")
+        pw = input(c.P + "\nEnter master password: " + c.E)
         if unlock(result[0]) == pw:
-            print(" + Access granted")
+            print(c.G + "\n- Access granted -" + c.E)
             return True
         else:
-            print("Access denied")
+            print(c.R + "\n! Access denied !" + c.E)
             return False
         
+
 
 # ---------- pass manager feaatures
 
 def addp():
-    site = input("Site/App name: ")
-    user = input("Username: ")
-    pw = input("Password: ")
-    cur.excute(r"INSERT INTO passwords (site, username, password) VALUES (%s, %s, %s)",
+    site = input(c.P + "Site/App name: " + c.E)
+    user = input(c.P + "Username: " + c.E)
+    pw = input(c.P + "Password: " + c.E)
+    cur.execute(r"INSERT INTO passwords (site, username, password) VALUES (%s, %s, %s)",
                (site, user, lock(pw)))
     con.commit()
-    print(" + Password added successfully")
+    print(c.G + "\n- Password added successfully -" + c.E)
 
 def viewp():
     cur.execute("SELECT id, site, username, password FROM passwords")
     rows = cur.fetchall()
     for r in rows:
-        print(f"\nID: {r[0]} \nSite: {r[1]} \nUser: {r[2]} \nPassword: {unlock(r[3])}")
+        print(c.BD + f"\nID       : {r[0]}" + c.E, f"\n-------------------------- \nSite     : {r[1]} \nUser     : {r[2]} \nPassword : {unlock(r[3])} \n")
 
 def updp():
-    id_ = input('Enter ID to update: ')
-    npw = input("New Password: ")
-    cur.execute(r"UPDATE passwords WHERE id=%s", (lock(npw), id_))
+    id_ = input(c.P + 'Enter ID to update: ' + c.E)
+    npw = input(c.P + "New Password: " + c.E)
+    cur.execute(r"UPDATE passwords SET password=%s WHERE id=%s", (lock(npw), id_))
     con.commit()
-    print("+ Password updated")
+    print(c.G + "\n- Password updated -" + c.E)
 
 def delp():
-    id_ = input("Enter ID to delete: ")
+    id_ = input(c.P + "Enter ID to delete: " + c.E)
     cur.execute(r"DELETE FROM passwords WHERE id=%s", (id_,))
     con.commit()
-    print(" + Password deleted")
+    print(c.R + "\n-! Password deleted !-" + c.E)
+
 
 
 # ---------- menu
@@ -131,7 +150,7 @@ def main():
         return
     
     while True:
-        print(r'''
+        print(c.B + r'''
 ______         _           _   _                _  _   
 | ___ \       | |         | | | |              | || |  
 | |_/ / _   _ | |_   ___  | | | |  __ _  _   _ | || |_ 
@@ -140,16 +159,17 @@ ______         _           _   _                _  _
 \____/  \__, | \__| \___|  \___/  \__,_| \__,_||_| \__|
          __/ |                                       
         |___/                                                                     
-''')
-        print('''
+''' + c.E)
+        
+        print(c.Y + '''
 1. Add Password
 2. View Password
 3. Update Password
 4. Delete Password
 5. Exit
-''')
+''' + c.E)
         
-        ch = input("Enter your choice: ")
+        ch = input(c.P + "Enter your choice: " + c.E)
 
         if ch == '1':
             addp()
@@ -160,10 +180,10 @@ ______         _           _   _                _  _
         elif ch == '4':
             delp()
         elif ch == '5':
-            print("Exiting ByteVault...")
+            print(c.G + "Exiting ByteVault..." + c.E)
             break
         else:
-            print("Invalid choice. Try again!")
+            print(c.R + "\n! Invalid choice. Try again! !" + c.E)
 
 if __name__ == "__main__":
     main()
